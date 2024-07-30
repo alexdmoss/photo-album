d1d4d7d7417b8b015c4efe42b88af06616aecfcd
Alex M <alex@alexos.dev>
2024-07-30 23:34:05 +0100

    Enable use of GCSFuse

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../k8s/" >/dev/null
 
 echo "-> [INFO] Applying Kubernetes yaml"
 
-kubectl apply -f ../namespace.yaml
+kubectl apply -f ./namespace.yaml
 kustomize edit set image "${SERVICE}"="${IMAGE_TAG}"
-kustomize build . | envsubst "\$GCP_PROJECT_ID \$AUTH_PROJECT_ID \$ALLOWED_USERS" | kubectl apply -n "${APP_NAME}" -f -
+kustomize build . | envsubst "\$GCP_PROJECT_ID \$AUTH_PROJECT_ID \$ALLOWED_USERS \$BUCKET_NAME" | kubectl apply -n "${APP_NAME}" -f -
 kubectl rollout status deploy/"${APP_NAME}"-"${SERVICE}" -n "${APP_NAME}" --timeout=300s
 
 popd >/dev/null

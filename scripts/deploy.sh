727aa2fe98eaac908288fc5b6e4fe9e7bab349f6
Alex M <alex@alexos.dev>
2024-07-30 23:17:17 +0100

    Switch to GKE

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,33 +1,14 @@
 #!/usr/bin/env bash
 set -euoE pipefail
 
-pushd "$(dirname "${BASH_SOURCE[0]}")/../terraform/" >/dev/null
+pushd "$(dirname "${BASH_SOURCE[0]}")/../k8s/" >/dev/null
 
-terraform init -backend-config=bucket="${GCP_PROJECT_ID}"-apps-tfstate -backend-config=prefix="${APP_NAME}"
 
-if [[ ${CI_SERVER:-} == "yes" ]]; then
-    IMAGE_TAG=${IMAGE_NAME}:${CI_COMMIT_SHA}-$(echo "${CI_COMMIT_TIMESTAMP}" | sed 's/[:+]/./g')
-    terraform apply -auto-approve -var gcp_project_id="${GCP_PROJECT_ID}" \
-        -var app_name="${APP_NAME}" \
-        -var image_tag="${IMAGE_TAG}" \
-        -var region="${REGION}" \
-        -var domain="${DOMAIN}" \
-        -var port="${PORT}" \
-        -var photos_bucket="${PHOTOS_BUCKET}" \
-        -var photos_project_id="${PHOTOS_PROJECT_ID}" \
-        -var auth_project_id="${AUTH_PROJECT_ID}" \
-        -var allowed_users="${ALLOWED_PHOTO_USERS}"
-else
-    terraform plan -var gcp_project_id="${GCP_PROJECT_ID}" \
-        -var app_name="${APP_NAME}" \
-        -var image_tag="${IMAGE_TAG}" \
-        -var region="${REGION}" \
-        -var domain="${DOMAIN}" \
-        -var port="${PORT}" \
-        -var photos_bucket="${PHOTOS_BUCKET}" \
-        -var photos_project_id="${PHOTOS_PROJECT_ID}" \
-        -var auth_project_id="${AUTH_PROJECT_ID}" \
-        -var allowed_users="${ALLOWED_PHOTO_USERS}"
-fi
+echo "-> [INFO] Applying Kubernetes yaml"
+
+kubectl apply -f ../namespace.yaml
+kustomize edit set image "${SERVICE}"="${IMAGE_TAG}"
+kustomize build . | envsubst "\$GCP_PROJECT_ID \$AUTH_PROJECT_ID \$ALLOWED_USERS" | kubectl apply -n "${APP_NAME}" -f -
+kubectl rollout status deploy/"${APP_NAME}"-"${SERVICE}" -n "${APP_NAME}" --timeout=300s
 
 popd >/dev/null

007220d29da4c787f57d075b0333951847e294b1
Alex M <alex@alexos.dev>
2024-07-30 23:39:17 +0100

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../k8s/" >/dev/null
 echo "-> [INFO] Applying Kubernetes yaml"
 
 kubectl apply -f ./namespace.yaml
-kustomize edit set image "${SERVICE}"="${IMAGE_TAG}"
+kustomize edit set image "${APP_NAME}"="${IMAGE_TAG}"
 kustomize build . | envsubst "\$GCP_PROJECT_ID \$AUTH_PROJECT_ID \$ALLOWED_USERS \$BUCKET_NAME" | kubectl apply -n "${APP_NAME}" -f -
-kubectl rollout status deploy/"${APP_NAME}"-"${SERVICE}" -n "${APP_NAME}" --timeout=300s
+kubectl rollout status deploy/"${APP_NAME}" -n "${APP_NAME}" --timeout=300s
 
 popd >/dev/null

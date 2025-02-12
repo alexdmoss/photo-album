a92d2ef2097864d83dd0440f5ff450abf0f6f106
Alex M <alex@alexos.dev>
2025-02-12 20:11:26 +0000

    Restore previous generated CSS to repair site. Not sure what is up with new version

--- run-local.sh
+++ run-local.sh
@@ -5,6 +5,7 @@ if [[ ${1:-} == "--docker" ]]; then
     docker run --rm -p 8000:8000 \
         -e GOOGLE_APPLICATION_CREDENTIALS="/app/.config/gcloud/application_default_credentials.json" \
         --env GOOGLE_CLOUD_PROJECT="$DATA_PROJECT_ID" \
+        --env DATA_PROJECT_ID="$DATA_PROJECT_ID" \
         --mount type=bind,source="$HOME"/.config/gcloud,target=/app/.config/gcloud \
         photo-album:latest
 else
